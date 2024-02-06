829e8bbbae25416726f5e8655436193edda9d99b
PHUPH41622 <143483485+PHUPH41622@users.noreply.github.com>
2024-02-06 20:53:15 +0700

    Initial commit

---
+++ src/main/java/Ex3_6513012/Newmain3.java
@@ -0,0 +1,139 @@
+package Ex3_6513012;
+
+//Napasrapee Satittham + 6513012
+
+import java.io.File;
+import java.util.*;
+
+public class Newmain3 {
+    public static void main(String[] args) {
+        Newmain3 mainapp = new Newmain3();
+        String path = "src/main/java/Ex3_6513012/";
+        String fileName = "languages.txt";
+        ArrayList<Language> AL = new ArrayList<>();
+        Scanner keyboardScan = new Scanner(System.in);
+        try {
+            Scanner filescan = new Scanner(new File(path + fileName));
+
+            for (int i = 0; filescan.hasNext(); i++) {
+                String line = filescan.nextLine();
+                String[] col = line.split(",");
+                if (i == 0) {
+                    continue;
+                }
+                Language l = new Language(col[0].trim(), Integer.parseInt(col[1].trim()), Double.parseDouble(col[2].trim()), Double.parseDouble(col[3].trim()), Double.parseDouble(col[4].trim()));
+                AL.add(l);
+            }
+        } catch (Exception e) {
+            System.out.print(e + " --> ");
+            System.out.println("New file name = ");
+            fileName = keyboardScan.next();
+        }
+        System.out.println("Sort by >> n = name");
+        System.out.println("           y = year");
+        System.out.println("           q = Stackoverflow question");
+        System.out.println("           r = GitHub repos");
+        System.out.println("           s = GitHub stars");
+        System.out.println("      others = quit");
+        String mode = (keyboardScan.next()).toLowerCase();
+        System.out.println("Language          Release Year    Stackoverflow Question(M)    GitHub Repos(M)   GitHub Top Stars(K)");
+        System.out.println("========================================================================================================");
+
+        switch (mode) {
+            case "n":
+                AL.sort(new SortLanguageByName());
+                for (Language l : AL) l.print();
+                break;
+            case "y":
+                mainapp.Ysort(AL);
+                break;
+            case "q":
+                mainapp.Qsort(AL);
+                break;
+            case "r":
+                mainapp.Rsort(AL);
+                break;
+            case "s":
+                mainapp.Ssort(AL);
+                break;
+            default:
+                System.out.println("A problem was encountered !!");
+        }
+    }
+
+    public void Ysort(ArrayList<Language> AL) {
+        Collections.sort(AL, new SortLanguageByYear()
+                .thenComparing( new SortLanguageByStackoverflow().reversed())
+                .thenComparing( new SortLanguageByGithubRepos().reversed())
+                .thenComparing( new SortLanguageByGithubStars().reversed())
+                .thenComparing( new SortLanguageByName())
+        );
+        for (Language l : AL) l.print();
+    }
+
+    public void Qsort(ArrayList<Language> AL) {
+        Collections.sort(AL, new SortLanguageByStackoverflow().reversed()
+                .thenComparing( new SortLanguageByGithubRepos())
+                .thenComparing( new SortLanguageByGithubStars())
+                .thenComparing( new SortLanguageByName())
+        );
+        for (Language l : AL) l.print();
+    }
+
+    public void Rsort(ArrayList<Language> AL) {
+        Collections.sort(AL, new SortLanguageByGithubRepos().reversed()
+                .thenComparing( new SortLanguageByGithubStars())
+                .thenComparing( new SortLanguageByName())
+        );
+        for (Language l : AL) l.print();
+    }
+
+    public void Ssort(ArrayList<Language> AL) {
+        Collections.sort(AL, new SortLanguageByGithubStars().reversed()
+                .thenComparing( new SortLanguageByName())
+        );
+        for (Language l : AL) l.print();
+    }
+}
+
+class Language {
+    private String name;
+    private int releaseYear;
+    private double stackQuestions; // Stackoverflow questions in millions
+    private double githubRepos; // GitHub repositories in millions
+    private double githubTopStars; // GitHub top-repo stars in thousands
+
+    public Language(String name, int year, double question, double repo, double star) {
+        this.name = name;
+        releaseYear = year;
+        stackQuestions = question;
+        githubRepos = repo;
+        githubTopStars = star;
+    }
+
+    public String getName()              { return name; }
+    public int    getReleaseYear()       { return releaseYear; }
+    public double getStackQuestions()    { return stackQuestions; }
+    public double getGithubRepos()       { return githubRepos; }
+    public double getGithubTopStars()    { return githubTopStars; }
+
+    public void print() {
+        System.out.printf("%-22s%-22d%-23.1f%-20.1f%.1f\n", this.name, releaseYear, stackQuestions, githubRepos, githubTopStars);
+    }
+}
+
+class SortLanguageByName implements Comparator<Language> {
+    public int compare(Language l1, Language l2)      { return l1.getName().compareToIgnoreCase(l2.getName()); }
+}
+class SortLanguageByYear implements Comparator<Language> {
+    public int compare(Language l1, Language l2)      { return l1.getReleaseYear() - l2.getReleaseYear(); }
+}
+class SortLanguageByStackoverflow implements Comparator<Language> {
+    public int compare(Language l1, Language l2)      { return Double.compare(l1.getStackQuestions(), l2.getStackQuestions()); }
+}
+class SortLanguageByGithubRepos implements Comparator<Language> {
+    public int compare(Language l1, Language l2)      { return Double.compare(l1.getGithubRepos(), l2.getGithubRepos()); }
+}
+class SortLanguageByGithubStars implements Comparator<Language> {
+    public int compare(Language l1, Language l2)      { return Double.compare(l1.getGithubTopStars(), l2.getGithubTopStars()); }
+}
---
+++ src/main/java/Ex4_6513012/Newmain4.java
@@ -0,0 +1,175 @@
+package Ex4_6513012;
+
+// Napasrapee Satittham 6513012
+
+import java.util.*;
+
+public class Newmain4 {
+    public static void main(String[] args) {
+        PriorityQueue<Customer> orderQueue = new PriorityQueue<>( new SortCustomerbyOrder().reversed()
+                                                   .thenComparing(new SortCustomerbyID())
+                                                                );
+        Random random = new Random();
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter refill items : ");
+        int refill = scan.nextInt();
+        System.out.println("Enter max days : ");
+        int days = scan.nextInt();
+
+        System.out.println("\n=== Day 0 : Customer arrival ===");
+        for(int j = 1; j <= 5; j++) {
+            int order = random.nextInt(20) + 1;
+            orderQueue.add( new Customer(j, order));
+            Customer c = new Customer(j, order);
+            c.print();
+            System.out.println();
+        }
+
+        Shop info = new Shop(refill, days, orderQueue);
+        info.simulation();
+    }
+}
+
+class Customer {
+    private static int runningID = 1; // for running customer ID
+    private int ID;
+    private int order; // order amount (random value 1-20)
+
+    public Customer(int ID, int order) {
+        this.ID = ID;
+        this.order = order;
+    }
+
+    public int getOrder() {
+        return order;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public void print() {
+        System.out.printf("[Customer  %-2d  order %2d lots]", ID, order);
+    }
+}
+
+class Shop {
+    Customer c;
+    Random random = new Random();
+    private int itemsToRefill; // items to refill in odd day
+    private int maxDays; // max day for simulation
+    int order;
+    private PriorityQueue<Customer> orderQueue = new PriorityQueue<>( new SortCustomerbyOrder().reversed()
+                                                       .thenComparing(new SortCustomerbyID())
+                                                                    );
+    private ArrayDeque<Customer> billingQueue = new ArrayDeque<>();
+
+    public Shop(int refill, int days, PriorityQueue orderQueue) {
+        itemsToRefill = refill;
+        maxDays = days;
+        this.orderQueue = orderQueue;
+    }
+
+    public void simulation() {
+        System.out.println();
+        System.out.println("=== Simulation ===");
+        int RefillPerOddDay = 0;
+        for(int day = 1; day <= maxDays; day++) {
+            if (day % 2 != 0) {
+                System.out.println("Day "+day);
+                RefillPerOddDay = RefillPerOddDay + itemsToRefill;
+                System.out.println("Refilling    >> Remaining items = "+RefillPerOddDay+" lots");
+                order = random.nextInt(20) + 1;
+                orderQueue.add( new Customer(5+day, order) );
+                System.out.print("New arrival  >> ");
+                c = new Customer(5+day, order);
+                c.print();
+                System.out.println();
+                System.out.print("Packing 1    >> ");
+                orderQueue.peek().print();
+                int rem = RemainCheck(RefillPerOddDay, orderQueue.peek().getOrder(), 1);
+                if (RefillPerOddDay == rem) {
+                    billingQueue.addLast(orderQueue.poll());
+                    System.out.print("Packing 2    >> ");
+                    orderQueue.peek().print();
+                    RefillPerOddDay = RemainCheck(RefillPerOddDay, orderQueue.peek().getOrder(), 2);
+                    orderQueue.add(billingQueue.pollLast());
+                    System.out.printf("\n\n");
+                    continue;
+                }
+                RefillPerOddDay = rem;
+                System.out.println();
+                System.out.print("Packing 2    >> ");
+                orderQueue.peek().print();
+                RefillPerOddDay = RemainCheck(RefillPerOddDay, orderQueue.peek().getOrder(), 2);
+                System.out.printf("\n\n");
+            } else {
+                System.out.println("Day "+day);
+                order = random.nextInt(20) + 1;
+                orderQueue.add( new Customer(5+day, order) );
+                System.out.print("New arrival  >> ");
+                c = new Customer(5+day, order);
+                c.print();
+                System.out.println();
+                System.out.print("Packing 1    >> ");
+                orderQueue.peek().print();
+                int rem = RemainCheck(RefillPerOddDay, orderQueue.peek().getOrder(), 1);
+                if (RefillPerOddDay == rem) {
+                    billingQueue.addFirst(orderQueue.poll());
+                    System.out.print("Packing 2    >> ");
+                    orderQueue.peek().print();
+                    RefillPerOddDay = RemainCheck(RefillPerOddDay, orderQueue.peek().getOrder(), 2);
+                    orderQueue.add(billingQueue.pollFirst());
+                    for (Customer c : billingQueue) {
+                        System.out.println("Billing      >> Customer "+billingQueue.poll().getID());
+                    }
+                    System.out.printf("\n");
+                    continue;
+                }
+                RefillPerOddDay = rem;
+                System.out.println();
+                System.out.print("Packing 2    >> ");
+                orderQueue.peek().print();
+                RefillPerOddDay = RemainCheck(RefillPerOddDay, orderQueue.peek().getOrder(), 2);
+                for (Customer c : billingQueue) {
+                    System.out.println("Billing      >> Customer "+billingQueue.poll().getID());
+                }
+                System.out.printf("\n");
+            }
+        }
+        System.out.println("\n=== Remaining customers in order queue ===");
+        while (orderQueue.size() > 0) {
+            orderQueue.poll().print();
+            System.out.println();
+        }
+        System.out.println("\n=== Remaining customers in billing queue (latest to earliest) ===");
+        Deque<Customer> billingQueueReverse = new ArrayDeque<>(billingQueue);
+        while (!billingQueueReverse.isEmpty()) {
+            billingQueueReverse.pollLast().print();
+            System.out.println();
+        }
+    }
+    private int RemainCheck(int RefillPerOddDay, int order, int pack) {
+        if (RefillPerOddDay - order >= 0) {
+            RefillPerOddDay = RefillPerOddDay - order;
+            System.out.printf("  success      Remaining items = %4d lots", RefillPerOddDay);
+            billingQueue.add(orderQueue.poll());
+            return RefillPerOddDay;
+        } else {
+            System.out.println("  failure");
+            return RefillPerOddDay;
+        }
+    }
+}
+
+class SortCustomerbyOrder implements Comparator<Customer> {
+    public int compare(Customer c1, Customer c2) {
+        return c1.getOrder() - c2.getOrder();
+    }
+}
+
+class SortCustomerbyID implements Comparator<Customer> {
+    public int compare(Customer c1, Customer c2) {
+        return c1.getID() - c2.getID();
+    }
+}
